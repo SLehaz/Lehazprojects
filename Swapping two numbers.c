4fbb3eba9249858f68022b3a8f743339ba5a8ed2
SLehaz <94469767+SLehaz@users.noreply.github.com>
2021-11-17 23:20:24 +0530

    create swapping two numbers
    this program shows swapping of two numbers

---
+++ Swapping two numbers.c
@@ -0,0 +1,8 @@
+int main()
+{   int a,b;
+    printf("Enter two numbers : ");
+    scanf("%d %d",&a,&b);
+    printf("Numbers before swapping : %d %d\n",a,b);
+    printf("Numbers after swapping : %d %d",b,a);
+    return 0;
+}
